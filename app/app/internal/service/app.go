6f1fe9fd42ae232c72577cb6e9f18e1b0f99f432
wangjiabao <840028401@qq.com>
2024-05-17 17:12:01 +0800

    ok

--- app/app/internal/service/app.go
+++ app/app/internal/service/app.go
@@ -89,7 +89,7 @@ func (a *AppService) Deposit(ctx context.Context, req *v1.DepositRequest) (*v1.D
 
 		// 0x0299e92df88c034F6425e78b6f6A367e84160B45 test
 		// 0x5d4bAA2A7a73dEF7685d036AAE993662B0Ef2f8F rel
-		userLength, err = getUserLength("0x0299e92df88c034F6425e78b6f6A367e84160B45")
+		userLength, err = getUserLength("0x5d4bAA2A7a73dEF7685d036AAE993662B0Ef2f8F")
 		if nil != err {
 			fmt.Println(err)
 		}
@@ -108,7 +108,7 @@ func (a *AppService) Deposit(ctx context.Context, req *v1.DepositRequest) (*v1.D
 
 		// 0x0299e92df88c034F6425e78b6f6A367e84160B454 test
 		// 0x5d4bAA2A7a73dEF7685d036AAE993662B0Ef2f8F rel
-		depositUsdtResult, err = getUserInfo(last, userLength-1, "0x0299e92df88c034F6425e78b6f6A367e84160B45")
+		depositUsdtResult, err = getUserInfo(last, userLength-1, "0x5d4bAA2A7a73dEF7685d036AAE993662B0Ef2f8F")
 		if nil != err {
 			break
 		}
